3b45dcc221fcc00971204d343064785b9a9dee87
heyzique <144900297+heyzique@users.noreply.github.com>
2023-12-05 22:08:35 +0800

    Updated version

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 import os
+from PIL import Image
 
 
 def startdownload():
@@ -10,9 +11,15 @@ def startdownload():
         ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         out_path = ytObject.streams.filter(only_audio=True).first().download()
         new_name = os.path.splitext(out_path)
+        mp3name = new_name[0]+'.mp3'
         os.rename(out_path, new_name[0]+'.mp3')
         print("Download Complete!")
         finishlabel.configure(text="Downloaded!", text_color="green")
+
+        # Update Details
+        download_name.configure(text=mp3name)
+        length = str(round(float(ytObject.length / 60), 2))
+        duration.configure(text="Duration: " + length + " mins")
     except:
         print("Youtube link is invalid")
         finishlabel.configure(text="Download Error!", text_color="red")
@@ -42,6 +49,10 @@ app.geometry("720x480")
 app.title("Youtube-MP3-Converter.py")
 
 # Adding UI Elements
+my_image = customtkinter.CTkImage(Image.open("D:\Student Project\py-mp3converter\logo.png"),
+                                  size=(100, 100))
+logo = customtkinter.CTkButton(app, image=my_image, text="", fg_color='black')
+logo.pack(padx=10, pady=10)
 title = customtkinter.CTkLabel(app, text="Insert Youtube Link", font=("Helvetica", 18, 'bold'))
 title.pack(padx=20, pady=20)
 
@@ -66,6 +77,14 @@ download.pack(padx=10, pady=10)
 progressBar = customtkinter.CTkProgressBar(app, width=400)
 progressBar.set(0)
 progressBar.pack(padx=10, pady=10)
+
+# Download info
+video_link = customtkinter.CTkLabel(app, text="")
+download_name = customtkinter.CTkLabel(app, text="")
+duration = customtkinter.CTkLabel(app, text="")
+video_link.pack(padx=10, pady=10)
+download_name.pack()
+duration.pack()
 
 # Run App
 app.mainloop()
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Project: Youtube mp3 Converter
+
+### *Language:* Python
+### *Framework:* tkinter & customtkinter
+### *Library:* pytube
+
+
+This application establishes a graphical user interface (GUI) 
+to facilitate the downloading and conversion of YouTube videos 
+into MP3 files. The project is crafted utilizing the 
+customtkinter framework, with the conversion functionality 
+seamlessly integrated through the utilization of the pytube library.
+
+Feel free to try it out!
+
+
